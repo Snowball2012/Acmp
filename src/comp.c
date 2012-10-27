e76a5ec1cfd15591e8c6b874479671d445a0de7f
Snowball <snowball@live.ru>
2012-10-28 02:23:57 +0400

    compression done, decompression to go

--- src/comp.c
+++ src/comp.c
@@ -8,12 +8,18 @@
 #include "comp.h"
 #include "ppm.h"
 
+void BitPlusFollow (unsigned char bit, CFile * file, int bits2Follow) {
+	WriteBit(bit, file);
+	for(;bits2Follow>0; bits2Follow--)
+		WriteBit(!bit, file);
+}
 
 void Compress(char * infile, char * outfile)
 {
 	unsigned long long l, h, firstQtr, thirdQtr, half;
 	int divisor;
 	int weights[256+1]; /*first zero + next 256 char elements */
+	int aggresivity = DEFAULT_AGGR;
 	int i;
 
 	DFile dataFile = NewFile(infile, "r");
@@ -29,9 +35,11 @@ void Compress(char * infile, char * outfile)
 		weights[i] = i;
 	divisor = 256; 
 
-	while (true) {
+	while (1) {
+		unsigned long long qtr; 
 		unsigned char c = GetSymbol(&dataFile);
-		int old_l, old_h;
+		unsigned long long old_l, old_h;
+		int btf = 0;  /* bits to follow */
 		if(strcmp("eof", dataFile.status)) {
 			fclose(dataFile.desc);
 			FinishWrite(&compFile);
@@ -40,10 +48,27 @@ void Compress(char * infile, char * outfile)
 		old_l = l;
 		old_h = h;
 		l = old_l + weights[c-1]*(old_h-old_l + 1)/divisor;
-		h = old_l + weights[c]*(old_h-old_l + 1)/divisor - 1;          
+		h = old_l + weights[c]*(old_h-old_l + 1)/divisor - 1;      
+		qtr = (h-l)/4;
+		firstQtr = l + qtr;
+		half = firstQtr + qtr;
+		thirdQtr = half + qtr;
 		for (;;) {  /* Ph'nglui mglw'nafh Cthulhu R'lyeh wgah'nagl fhtagn (,;,;,) */
-			
-
+			if (h < half)
+				BitPlusFollow(0, &compFile, btf);
+			else if (l > half) {
+				BitPlusFollow(1, &compFile, btf);
+				l-=(qtr*2);
+				h-=(qtr*2);
+			} else if (l >= firstQtr && h < thirdQtr) {
+				btf++;
+				l-=qtr;
+				h-=qtr;
+			} else 
+				break;
+			l+=l;
+			h+=h+1;
+		}
+		divisor = RecalcWeight(weights, c, &aggresivity);
 	}
-
 }
